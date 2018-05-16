a264457d1a51fe20c64f75c8889a5a70108739e4
subba_gunjikunta <subba_gunjikunta@belk.com>
2018-05-16 17:03:18 -0400

    xml message with loop

--- lib/workhorse/color_master.rb
+++ lib/workhorse/color_master.rb
@@ -4,14 +4,24 @@ class ColorMaster
   
   def initialize(response)
     #@response = 
-    #binding.pry
+  #  binding.pry
      @product_id= response.id
-     @class= response.properties.find { |property| property.id == 'Class#' }['values'].first['id']
-     @dept= response.properties.find { |property| property.id == 'Dept#' }['values'].first['id']
-     @image_asset_source= response.properties.find { |property| property.id == 'ImageAssetSource' }['values'].first['id']
-     @turn_in_date= response.properties.find { |property| property.id == 'Turn-In Date' }['values'].first['id']
-     @vendor= response.properties.find { |property| property.id == 'Vendor#' }['values'].first['id']
-        #binding.pry
+     if !response.properties.find { |property| property.id == 'Class#' }.nil?
+       @class= response.properties.find { |property| property.id == 'Class#' }['values'].first['id']
+     end
+     if !response.properties.find { |property| property.id == 'Dept#' }.nil?
+       @dept= response.properties.find { |property| property.id == 'Dept#' }['values'].first['id']
+     end
+      if !response.properties.find { |property| property.id == 'ImageAssetSource' }.nil?
+        @image_asset_source= response.properties.find { |property| property.id == 'ImageAssetSource' }['values'].first['id']
+      end
+      if !response.properties.find { |property| property.id == 'Turn-In Date' }.nil?
+        @turn_in_date= response.properties.find { |property| property.id == 'Turn-In Date' }['values'].first['id']
+      end
+      if !response.properties.find { |property| property.id == 'Vendor#' }.nil?
+        @vendor= response.properties.find { |property| property.id == 'Vendor#' }['values'].first['id']
+      end
+         #binding.pry
   end
   
   # # def attributes
@@ -25,6 +35,9 @@ class ColorMaster
   #     }
   #   end
   # 
+  def product_id
+    @product_id
+  end
     def valid?
       turn_in_date? && sample_management?
     end
@@ -37,23 +50,7 @@ class ColorMaster
       @image_asset_source == 'Sample Management'
     end
     
-    def to_xml
-    ##  puts(attributes.dept)
-      builder = Nokogiri::XML::Builder.new do |xml|
-        xml.root {
-          xml.Project {
-            xml.ShotGroup('id' => @product_id, 'CollectionOrGroup' => 'N', 'SalsifyID' => '12345') {
-              xml.Image('id' => @product_id){
-               xml.Sample('deptnnum' => @dept ){
-                  xml.id_ "10"   
-                }
-              }
-            }
-          }
-        }
-      end
-      puts builder.to_xml
-    end
+    
   end
 
 
--- lib/workhorse/send_image_request.rb
+++ lib/workhorse/send_image_request.rb
@@ -1,6 +1,6 @@
 module Workhorse
   class SendImageRequest
-
+    
     MAX_IDS_PER_CRUD = 100.freeze
     PROPERTY_NRF_COLOR_CODE = 'nrfColorCode'.freeze
     PROPERTY_COLOR_MASTER = 'Color Master?'.freeze
@@ -12,67 +12,77 @@ module Workhorse
       'Turn-In Date',
       'ImageAssetSource'
     ].freeze
-
+    @photoRequests
     def self.run
       new.run
     end
-
+    
     def run
       photo_requests_to_send
       # TODO: Iterate over products_to_send and generate xml
+      to_xml(@photoRequests)
       # TODO: Send xml to workhorse   
       # TODO: Mark sample requests as sent to workhorse
     end
-
-    # def photo_requests_to_send
-    #   @photo_requests_to_send ||= unsent_sample_requests.group_by do |sample_request|
-    #     sample_request.product_id
-    #   end.map do |style_id, sample_request|
-    #       binding.pry
-    #     color_master = retrieve_color_master(style_id, sample_request.first.color_id).try(:first)
-    #       binding.pry
-    #     next unless color_master
-    #     color_master = ColorMaster.new(color_master)
-    #       binding.pry
-    #     #color_master1.attributes
-    #     #biniding.pry
-    #     next unless color_master.valid?
-    #     color_master.to_xml()
-    #   end.compact
-    # end
+    
+  
     def photo_requests_to_send
-      @photo_requests_to_send ||= unsent_sample_requests.group_by do |sample_request|
+      smapleRequests = []
+      getUnsentSampleRequest
+      filter="="
+              @photo_requests.each  do |sample_request|
+                # TODO need to limit the number of products else request may get fail
+                 #next unless color_master
+              filter+="'Parent Product':'"
+              filter+= sample_request["product_id"]
+              filter+="','nrfColorCode':'"
+              filter+=sample_request["color_id"].strip
+              filter+="','Color Master?':'true'="
+              end
+          filter.slice!(0,filter.length-1)
+          puts(filter)
+          @photoRequests = retrieve_color_master(filter)
+          puts(@photoRequests)
       
-        color_master = retrieve_color_master(sample_request.product_id, sample_request.color_id).try(:first)
-        #next unless color_master
-          color_master = ColorMaster.new(color_master)
-        color_master.to_xml()
-        end
-          
-        #color_master1.attributes
-        #biniding.pry
-        #unless color_master.valid?
-        #color_master.to_xml()
-        #end.compact
     end
     # def formatFilterRequest
     # 
     # end
-
-    def retrieve_color_master(style_id, nrf_color_code)
+    def to_xml(photoRequests)
+    ##  puts(attributes.dept)
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.root {
+          xml.Project {
+            photoRequests.each do |photo_request|
+            color_master=ColorMaster.new(photo_request)
+            xml.ShotGroup('id' => color_master.product_id, 'CollectionOrGroup' => 'N', 'SalsifyID' => '12345') {
+              xml.Image('id' => color_master.product_id){
+               xml.Sample('deptnnum' => '123'){
+                  xml.id_ "10"   
+                }
+              }
+            }
+          end
+          }
+        }
+      end
+      puts builder.to_xml
+    end
+    
+    def retrieve_color_master(filter)
       salsify.products_filtered_by(
-         filter: "='Parent Product':'#{style_id}','nrfColorCode':'#{nrf_color_code}','Color Master?':'true'",
-         selections: SELECTIONS
+        filter: filter,
+        selections: SELECTIONS
       ).products
     end
-
+    
     def unsent_sample_requests
       @unsent_sample_requests ||= [
         SampleRequest.where(sent_to_workhorse: false),
         SampleRequest.where(sent_to_workhorse: nil)
       ].flatten
     end
-
+    
     def salsify
       @salsify ||= Salsify::Client.create_with_token(
         ENV.fetch('SALSIFY_USER_EMAIL'),
@@ -80,6 +90,24 @@ module Workhorse
         organization_id: ENV.fetch('SALSIFY_ORG_SYSTEM_ID')
       )
     end
-
+    def getUnsentSampleRequest
+      response = RestClient::Request.new({
+        method: :get,
+        url: 'https://customer-belk-qa.herokuapp.com/api/workhorse/sample_requests?unsent=true',
+        headers: { :api_token => '4591D7EF39D6CFE0482778AACB8A0534B99DB31317D528E310373B1BC0E16E22' }
+      }).execute do |response, request, result|
+        case response.code
+        when 400
+            @photo_requests=JSON.parse(response.body)
+        when 200
+          @photo_requests=JSON.parse(response.body)
+        #  puts(@unsent_sample_requests)
+        #  [ :success, parse_json(response.to_str) ]
+        else
+          fail "Invalid response #{response.to_str} received."
+        end
+      end  
+    end
+    
   end
 end
