716b35b379c7e7a2ecbe32d4a25a2c9dd1eb6363
subba_gunjikunta <subba_gunjikunta@belk.com>
2018-05-25 11:31:38 -0400

    add space

--- lib/workhorse/send_image_request.rb
+++ lib/workhorse/send_image_request.rb
@@ -17,17 +17,21 @@ module Workhorse
     def self.run
       new.run
     end
-    
+
     def run
+
+      binding.pry
       photo_requests_to_send
       # TODO: Iterate over products_to_send and generate xml
+      binding.pry
       to_xml(@photoRequests)
-      # TODO: Send xml to workhorse  
-      transferFile 
+      # TODO: Send xml to workhorse
+      transferFile
+      binding.pry
       # TODO: Mark sample requests as sent to workhorse
     end
-    
-  
+
+
     def photo_requests_to_send
       smapleRequests = []
       getUnsentSampleRequest
@@ -41,14 +45,16 @@ module Workhorse
               filter+=sample_request["color_id"].strip
               filter+="','Color Master?':'true'="
               end
+              binding.pry
           filter.slice!(0,filter.length-1)
       #    puts(filter)
+           binding.pry
           @photoRequests = retrieve_color_master(filter)
       #    puts(@photoRequests)
-      
+
     end
     # def formatFilterRequest
-    # 
+    #
     # end
     def transferFile
       require 'net/sftp'
@@ -56,10 +62,11 @@ module Workhorse
        # upload a file or directory to the remote host
        binding.pry
       sftp.upload!(@fileName, File.join('SalsifyImportToWH',File.basename(@fileName)))
-      
+
+
     end
     end
-    
+
     def to_xml(photoRequests)
     ##  puts(attributes.dept)
       @fileName = '/Users/afusrg1/Subba/Ruby/belkApp/photoRequest/photoRequests_'+Time.now.strftime('%Y-%m-%d_%H-%M-%S')+'.xml'
@@ -71,7 +78,7 @@ module Workhorse
             xml.ShotGroup('id' => color_master.product_id, 'CollectionOrGroup' => 'N', 'SalsifyID' => '12345') {
               xml.Image('id' => color_master.product_id){
                xml.Sample('deptnnum' => '123'){
-                  xml.id_ "10"   
+                  xml.id_ "10"
                 }
               }
             }
@@ -79,24 +86,25 @@ module Workhorse
           }
         }
       end
+      binding.pry
       File.write(@fileName, builder.to_xml)
       puts builder.to_xml
     end
-    
+
     def retrieve_color_master(filter)
       salsify.products_filtered_by(
         filter: filter,
         selections: SELECTIONS
       ).products
     end
-    
+
     def unsent_sample_requests
       @unsent_sample_requests ||= [
         SampleRequest.where(sent_to_workhorse: false),
         SampleRequest.where(sent_to_workhorse: nil)
       ].flatten
     end
-    
+
     def salsify
       @salsify ||= Salsify::Client.create_with_token(
         ENV.fetch('SALSIFY_USER_EMAIL'),
@@ -104,7 +112,7 @@ module Workhorse
         organization_id: ENV.fetch('SALSIFY_ORG_SYSTEM_ID')
       )
     end
-    
+
     def getUnsentSampleRequest
       response = RestClient::Request.new({
         method: :get,
@@ -121,8 +129,8 @@ module Workhorse
         else
           fail "Invalid response #{response.to_str} received."
         end
-      end  
+      end
     end
-    
+
   end
 end
